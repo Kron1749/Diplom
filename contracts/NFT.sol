c1bc70360c81f7239e3a33c404c9e1508303c696
Kron1749 <70746258+Kron1749@users.noreply.github.com>
2022-11-27 15:08:28 +0300

    Contract Updated

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,35 +5,44 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721,Ownable {
-
+contract NFT is ERC721, Ownable {
     string public baseTokenURI;
-    mapping(uint256=>string) public _tokenURIs;
+    uint256 public s_tokenId;
+    mapping(uint256 => string) public _tokenURIs;
 
-    constructor() ERC721("NFT_TOKEN_1","NFT1"){}
+    constructor() ERC721("NFT_TOKEN", "NFT") {
+        s_tokenId = 0;
+    }
 
     function setBaseURI(string memory _baseTokenURI) public onlyOwner {
         baseTokenURI = _baseTokenURI;
     }
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-		require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-		string memory _tokenURI = _tokenURIs[tokenId];
-		return _tokenURI;
-	}
 
-
-    function mintToken(
-		address _to,
-		uint256 _tokenId,
-		string memory tokenURI_
-	) public payable onlyOwner {
-		_safeMint(_to, _tokenId);
-		_setTokenURI(_tokenId, tokenURI_);
-	}
-
-    function _setTokenURI(uint256 tokenId,string memory _tokenURI) internal {
-        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
-		_tokenURIs[tokenId] = _tokenURI;
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        string memory _tokenURI = _tokenURIs[tokenId];
+        return _tokenURI;
     }
 
+    function mintToken(
+        address _to,
+        string memory tokenURI_
+    ) public payable onlyOwner {
+        _safeMint(_to, s_tokenId);
+        _setTokenURI(s_tokenId, tokenURI_);
+        s_tokenId++;
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI set of nonexistent token"
+        );
+        _tokenURIs[tokenId] = _tokenURI;
+    }
 }
